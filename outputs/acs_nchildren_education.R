b6f36a3e5fe4b30f26957aaf6f6a8ae0cd4e85e6
gustavomluchesi <gustavoml@uchicago.edu>
2024-10-17 01:23:24 -0500

    Added new figures
    Added figures by education attainment

--- outputs/acs_nchildren_education.R
+++ outputs/acs_nchildren_education.R
@@ -1,7 +1,7 @@
 ###########################################################################
 # Script: acs_nchildren_education.R
 # Author: Gustavo Luchesi
-# Last Updated: 10/14/2024
+# Last Updated: 10/16/2024
 # Description: Creates figures for number of children per education group
 
 # Input: acs_educ_groups.rdata
@@ -28,40 +28,148 @@ setwd("C:/Users/gustavoml/Desktop/Projects/FertilityFacts")
 load("refined/acs_educ_groups.rdata")
 
 #===========================================================================
-# Testing alternative ways to group fertility data by education attainment
+# Function for mean number of children in the HH by education group
 #===========================================================================
 
+hhchildren.lineplot <- function(data, group_var, start_age, end_age){
+  
+  grouped_data <- data %>% 
+    filter(!is.na(.data[[group_var]]),
+           AGE %in% c(start_age:end_age),
+           YEAR >= 2000) %>% 
+    group_by(YEAR, .data[[group_var]]) %>% 
+    summarise(n_child = weighted.mean(NCHILD, w = PERWT)) %>% 
+    ungroup()
+  
+  
+  grouped_data %>% 
+    ggplot(aes(x = YEAR, y = n_child, color = .data[[group_var]])) +
+    geom_point(size = 2.5) +
+    geom_line(linewidth = 0.75) +
+    scale_color_paletteer_d("ggthemes::Red_Blue_Brown") +
+    scale_x_continuous(breaks = seq(min(grouped_data$YEAR), max(grouped_data$YEAR), by = 2)) +
+    labs(title = paste0("Number of own children living in HH (Women aged ", start_age, "-", end_age, ")"),
+         x = "Year", color = "Education Group:") +
+    theme_minimal() +
+    theme(axis.title.y = element_blank(),
+          axis.text = element_text(size = 10),
+          axis.line = element_line(),
+          axis.ticks = element_line(),
+          panel.grid.major = element_line(linetype = "dashed"),
+          panel.grid.minor = element_blank(),
+          legend.position = "right",
+          plot.caption = element_text(hjust = 0))
+  
+}
+
+#===========================================================================
+# Generating line plots for different age groups
+#===========================================================================
+
+# Initial Classification
+
+start_ages <- c(45, 45, 43, 40, 45)
+end_ages <- c(45, 55, 47, 44, 50)
+
+for (i in 1:length(start_ages)) {
+  
+  lineplot <- hhchildren.lineplot(data_educ, "educ_group", start_age =  start_ages[i], end_age =  end_ages[i])
+  
+  figure_path <- paste0("figures/line_hhchildren_", start_ages[i], "_", end_ages[i], ".jpeg")
+  overleaf_path <- paste0("overleaf/line_hhchildren_", start_ages[i], "_", end_ages[i], ".jpeg")
+  ggsave(figure_path, plot = lineplot, dpi = 320, width = 8, height = 5)
+  ggsave(overleaf_path, plot = lineplot, dpi = 320, width = 8, height = 5)
+  
+}
+
+# Generating line plots for different age groups (Simplified Measure)
+
+for (i in 1:length(start_ages)) {
+  
+  lineplot <- hhchildren.lineplot(data_educ, "educ_group2", start_age =  start_ages[i], end_age =  end_ages[i])
+  
+  figure_path <- paste0("figures/line_hhchildren1_", start_ages[i], "_", end_ages[i], ".jpeg")
+  overleaf_path <- paste0("overleaf/line_hhchildren1_", start_ages[i], "_", end_ages[i], ".jpeg")
+  ggsave(figure_path, plot = lineplot, dpi = 320, width = 8, height = 5)
+  ggsave(overleaf_path, plot = lineplot, dpi = 320, width = 8, height = 5)
+  
+}
+
+# Generating line plots for different age groups (Doepke Measure)
+
+for (i in 1:length(start_ages)) {
+  
+  lineplot <- hhchildren.lineplot(data_educ, "educ_simplified", start_age =  start_ages[i], end_age =  end_ages[i])
+  
+  figure_path <- paste0("figures/line_hhchildren2_", start_ages[i], "_", end_ages[i], ".jpeg")
+  overleaf_path <- paste0("overleaf/line_hhchildren2_", start_ages[i], "_", end_ages[i], ".jpeg")
+  ggsave(figure_path, plot = lineplot, dpi = 320, width = 8, height = 5)
+  ggsave(overleaf_path, plot = lineplot, dpi = 320, width = 8, height = 5)
+  
+}
+
+#===========================================================================
+# Generating bar plots including specific years
+#===========================================================================
+
+# Simplified Education Measure
 grouped_data <- data_educ %>% 
-  
-  filter(!is.na(educ_group),
-         AGE %in% c(43:47)) %>% 
-  
-  group_by(YEAR, educ_group) %>% 
-  summarise(n_child = weighted.mean(NCHILD, w = PERWT),
-            n_sample = n(),
-            total_weight = sum(PERWT)) %>% 
+  mutate(educ_group2 = fct_relevel(educ_group2, c("Less than college", "Some college", "Bachelor's",
+                                   "Master's (Lawyer/Doctor)", "Master's", "PhD"))) %>% 
+  filter(YEAR %in% c(1990, 2000, 2010, 2020),
+         AGE %in% c(45:50)) %>% 
+  group_by(YEAR, educ_group2) %>% 
+  summarise(hh_nchildren = weighted.mean(NCHILD, w = PERWT)) %>% 
   ungroup()
 
-#===========================================================================
-# Creating figure of number of children by education group
-#===========================================================================
-
-grouped_data %>% 
-  ggplot(aes(x = YEAR, y = n_child, color = educ_group)) +
-  geom_point(size = 2.5) +
-  geom_line(linewidth = 0.75) +
-  scale_x_continuous(limits = c(2009, 2022), 
-                     breaks = seq(2009, 2022, 2)) +
-  scale_color_paletteer_d("ggthemes::Red_Blue_Brown") +
-  labs(title = "Number of own children living in HH",
-       x = "Year", color = "Education Group:") +
+col_plot <- grouped_data %>% 
+  ggplot(aes(x = educ_group2, y = hh_nchildren, fill = as.factor(YEAR))) +
+  geom_col(position = "dodge") +
+  scale_fill_brewer(palette = "Paired") +
+  labs(fill = "Year:",
+       title = "Mean number of own children in HH (Women aged 45-50)") +
   theme_minimal() +
-  theme(axis.title.y = element_blank(),
+  theme(legend.position = "bottom",
         axis.text = element_text(size = 10),
         axis.line = element_line(),
         axis.ticks = element_line(),
-        panel.grid.major = element_line(linetype = "dashed"),
-        panel.grid.minor = element_blank(),
-        legend.position = "right",
-        plot.caption = element_text(hjust = 0))
+        axis.title.y = element_blank(),
+        axis.title.x = element_blank(),
+        panel.grid = element_blank())
+
+figure2_path <- "figures/barplot_hhchildren_45_50.jpeg"
+overleaf2_path <- "overleaf/barplot_hhchildren_45_50.jpeg"
+ggsave(figure2_path, plot = col_plot, dpi = 320, width = 8, height = 5)
+ggsave(overleaf2_path, plot = col_plot, dpi = 320, width = 8, height = 5)
+
+# Doepke Measure
+
+grouped_data2 <- data_educ %>% 
+  mutate(educ_simplified = fct_relevel(educ_simplified, c("< 12", "12", "13-15", "16", "> 16"))) %>% 
+  filter(YEAR %in% c(1990, 2000, 2010, 2020),
+         AGE %in% c(45:50)) %>% 
+  group_by(YEAR, educ_simplified) %>% 
+  summarise(hh_nchildren = weighted.mean(NCHILD, w = PERWT)) %>% 
+  ungroup()
+
+col_plot2 <- grouped_data2 %>% 
+  ggplot(aes(x = educ_simplified, y = hh_nchildren, fill = as.factor(YEAR))) +
+  geom_col(position = "dodge") +
+  scale_fill_brewer(palette = "Paired") +
+  labs(fill = "Year:",
+       x = "Years of Schooling",
+       title = "Mean number of own children in HH (Women aged 45-50)") +
+  theme_minimal() +
+  theme(legend.position = "bottom",
+        axis.text = element_text(size = 10),
+        axis.line = element_line(),
+        axis.ticks = element_line(),
+        axis.title.y = element_blank(),
+        panel.grid = element_blank())
+
+figure3_path <- "figures/barplot_hhchildren1_45_50.jpeg"
+overleaf3_path <- "overleaf/barplot_hhchildren1_45_50.jpeg"
+ggsave(figure3_path, plot = col_plot2, dpi = 320, width = 8, height = 5)
+ggsave(overleaf3_path, plot = col_plot2, dpi = 320, width = 8, height = 5)
+
 
--- source/extract_acs.R
+++ source/extract_acs.R
@@ -1,7 +1,7 @@
 ###########################################################################
 # Script: extract_acs.R
 # Author: Gustavo Luchesi
-# Last Updated: 10/14/2024
+# Last Updated: 10/16/2024
 # Description: Extracts ACS PUMS dat file from raw and creates intermediate datasets
 # based on the American Community Survey PUMS
 
@@ -88,45 +88,44 @@ data_educ <- data_educ %>%
                                 EDUCD %in% c(81:100) ~ "Associate's degree",
                                 EDUCD == 101 & stem_degree == 0 ~ "Non-STEM Bachelor's",
                                 EDUCD == 101 & stem_degree == 1 ~ "STEM Bachelor's",
-                                EDUCD %in% c(114:115) & OCC2010 == 2100 ~ "Master's and Lawyer",
-                                EDUCD %in% c(114:115) & OCC2010 == 3060 ~ "Master's and Doctor",
-                                EDUCD %in% c(114:115) & !OCC2010 %in% c(2100, 3060) & stem_degree == 1 ~ "Master's and STEM major",
-                                EDUCD %in% c(114:115) & !OCC2010 %in% c(2100, 3060) & stem_degree == 0 ~ "Master's and non-STEM major",
+                                EDUCD %in% c(114:115) & OCC2010 == 2100 ~ "Master's (Lawyer)",
+                                EDUCD %in% c(114:115) & OCC2010 == 3060 ~ "Master's (Doctor)",
+                                EDUCD %in% c(114:115) & !OCC2010 %in% c(2100, 3060) & stem_degree == 1 ~ "Master's and STEM",
+                                EDUCD %in% c(114:115) & !OCC2010 %in% c(2100, 3060) & stem_degree == 0 ~ "Master's and non-STEM",
                                 EDUCD == 116 ~ "PhD"),
-         educ_group = fct_relevel(educ_group, c("PhD", "Master's and non-STEM major", "Master's and STEM major", 
-                                                "Master's and Doctor", "Master's and Lawyer", "STEM Bachelor's",
+         educ_group = fct_relevel(educ_group, c("PhD", "Master's and non-STEM", "Master's and STEM", 
+                                                "Master's (Doctor)", "Master's (Lawyer)", "STEM Bachelor's",
                                                 "Non-STEM Bachelor's", "Associate's degree", "Some college", 
                                                 "Less than college")),
          
          educ_group1 = case_when(EDUCD <= 64 ~ "Less than college",
-                                 EDUCD %in% c(65:100) ~ "Some college or Associate's degree",
-                                 EDUCD == 101 ~ "Bachelor's degree",
-                                 EDUCD %in% c(114:115) & OCC2010 == 2100 ~ "Master's and Lawyer",
-                                 EDUCD %in% c(114:115) & OCC2010 == 3060 ~ "Master's and Doctor",
-                                 EDUCD %in% c(114:115) & !OCC2010 %in% c(2100, 3060) ~ "Master's degree (except Lawyers and Doctors)",
+                                 EDUCD %in% c(65:100) ~ "Some college",
+                                 EDUCD == 101 ~ "Bachelor's",
+                                 EDUCD %in% c(114:115) & OCC2010 == 2100 ~ "Master's (Lawyer)",
+                                 EDUCD %in% c(114:115) & OCC2010 == 3060 ~ "Master's (Doctor)",
+                                 EDUCD %in% c(114:115) & !OCC2010 %in% c(2100, 3060) ~ "Master's",
                                  EDUCD == 116 ~ "PhD"),
-         educ_group1 = fct_relevel(educ_group1, c("PhD", "Master's degree (except Lawyers and Doctors)",
-                                                  "Master's and Doctor", "Master's and Lawyer", "Bachelor's degree",
-                                                  "Some college or Associate's degree", "Less than college")),
+         educ_group1 = fct_relevel(educ_group1, c("PhD", "Master's",
+                                                  "Master's (Doctor)", "Master's (Lawyer)", "Bachelor's",
+                                                  "Some college", "Less than college")),
          
          educ_group2 = case_when(EDUCD <= 64 ~ "Less than college",
-                                 EDUCD %in% c(65:100) ~ "Some college or Associate's degree",
-                                 EDUCD == 101 ~ "Bachelor's degree",
-                                 EDUCD %in% c(114:115) & OCC2010 %in% c(2100, 3060) ~ "Master's and Lawyer or Doctor",
-                                 EDUCD %in% c(114:115) & !OCC2010 %in% c(2100, 3060) ~ "Master's degree (except Lawyers and Doctors)",
+                                 EDUCD %in% c(65:100) ~ "Some college",
+                                 EDUCD == 101 ~ "Bachelor's",
+                                 EDUCD %in% c(114:115) & OCC2010 %in% c(2100, 3060) ~ "Master's (Lawyer/Doctor)",
+                                 EDUCD %in% c(114:115) & !OCC2010 %in% c(2100, 3060) ~ "Master's",
                                  EDUCD == 116 ~ "PhD"),
-         educ_group2 = fct_relevel(educ_group2, c("PhD", "Master's degree (except Lawyers and Doctors)",
-                                                  "Master's and Lawyer or Doctor", "Bachelor's degree",
-                                                  "Some college or Associate's degree", "Less than college")),
+         educ_group2 = fct_relevel(educ_group2, c("PhD", "Master's",
+                                                  "Master's (Lawyer/Doctor)", "Bachelor's",
+                                                  "Some college", "Less than college")),
          
          educ_simplified = case_when(EDUC < 6 ~ "< 12",
                                      EDUC == 6 ~ "12",
                                      EDUC %in% c(7:9) ~ "13-15",
                                      EDUC == 10 ~ "16",
                                      EDUC == 11 ~ "> 16"),
-         educ_simplified = fct_relevel(educ_simplified, c("< 12", "12", "13-15", "16", "> 16"))) 
+         educ_simplified = fct_relevel(educ_simplified, c("> 16", "16", "13-15", "12", "< 12"))) 
 
 # Saving intermediate dataset
 path_educ = "refined/acs_educ_groups.rdata"
 save(data_educ, file = path_educ)
-
